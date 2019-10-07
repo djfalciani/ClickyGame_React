f2f65d30d31abea82149c232b56b793771dfc500
djfalciani <dfalciani@gmail.com>
2019-10-06 20:06:52 -0400

    style changes

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -5,8 +5,8 @@ function jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Fluid jumbotron</h1>
-                <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <h1 className="display-4">Clicky Game!</h1>
+                <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
             </div>
         </div>
     );
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,11 +4,12 @@ import "./style.css";
 function navbar(props) {
   return (
     // Traditional Bootstrap...
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="/">
-        Navbar
+        Clicky Game
       </a>
-      <p>Score: {props.score}  |  Top Score: {props.topScore}</p>
+      <p className="navText">Score: {props.score}  |  Top Score: {props.topScore}</p>
       
     </nav>
   );
